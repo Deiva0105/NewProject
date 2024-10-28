5184ae8f6f0913c00931a277143a0ac1013db07a
Deiva0105 <deivakarpagam.1998@gmail.com>
2024-10-28 18:44:15 +0530

    done

---
+++ Sample.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Sample {
+	
+	private void empName() {
+		System.out.println("Employee Name : Deiva");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.empName();
+		
+	}
+	
+	
+	
+
+}
